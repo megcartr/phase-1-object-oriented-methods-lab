8a7b9c23cc247b893f47e9f748dc227077396ab6
Meghan Carter <msc2010@yahoo.com>
2023-10-02 18:55:16 -0500

    lab completed

---
+++ index.js
@@ -0,0 +1,27 @@
+function BoardMember(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+  
+  BoardMember.prototype = {
+    veto: function () {
+      return "No, I must disagree";
+    },
+    approve: function () {
+      return "You can do that!";
+    },
+    doCharity: function () {
+      return "I like to help people.";
+    },
+    releasePressStatement: function () {
+      return "You will see great things from Scuber.";
+    },
+    sayHi: function () {
+      return "Hi, my name is " + this.name + ". I am from " + this.homeState + ", and I was trained in " + this.training + ".";
+    }
+  };
+  
+  var boardMember1 = new BoardMember("Ali", "Ahmed", "Muhammad");
+  console.log(boardMember1.sayHi());
+  console.log(boardMember1.approve());
